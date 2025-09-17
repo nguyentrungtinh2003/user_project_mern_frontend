1d60a46d1a9d1790910b396c4ba921741af877b4
Chapter Book <truongsach2003@gmail.com>
2025-09-17 23:26:20 +0700

    Update root

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -2,13 +2,18 @@ import { Provider } from "react-redux";
 import { ReduxRouter } from "../libs/redux-router";
 import router from "./routes";
 import { StrictMode } from "react";
+import { AuthProvider } from "./contexts/AuthContext";
+import { Toaster } from "./components/ui/sonner";
 
 const Root = ({ store, history }) => {
   return (
     <Provider store={store}>
       <ReduxRouter history={history}>
         <StrictMode>
-          <RouterProvider router={router} />
+          <AuthProvider>
+            <Toaster position="top-right" duration={1500} />
+            <RouterProvider router={router} />
+          </AuthProvider>
         </StrictMode>
       </ReduxRouter>
     </Provider>
